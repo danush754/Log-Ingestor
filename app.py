3e146cc0a657674115e3e2233b6dcc98e790267b
Dhanush <danush2228s@gmail.com>
2023-11-19 15:45:35 +0530

    Added new methods to get the logs and to store in a list Datastructure

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app  = Flask(__name__)
 
+logs = []
+
 @app.route('/ingest', methods=['POST'])
 def ingest():
-    log_data = request.data.decode('utf-8')
-    print(f"Recieved the log data: {log_data}")
-    return "Log ingestion successful :)"
+    try:
+
+        log_data = request.json 
+        if not isinstance(log_data,dict):
+            return jsonify({'error' : 'Invalid Json format'}),400
+        
+        logs.append(log_data)
+
+        return jsonify({'message' : 'Log ingestion successfull !'})
+    except Exception as e:
+        return jsonify({'error':str(e)}),500
+
+@app.route('/logs', methods=['GET'])
+def get_logs():
+    return jsonify({'logs' : logs})
+        
+        
+    
 
 if(__name__ == '__main__'):
     app.run(port=3000)
