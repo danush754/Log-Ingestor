08881aaf8cf0ab64724c5ef20adedabad3af8a61
dhan <danush2228s@gmail.com>
2024-07-21 11:54:08 +0530

    Deleted old files

--- test.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-testApp = Flask(__name__)
-
-@testApp.route('/')
-def index():
-    return "This is the test app to check my flask is running successfully"
-
-testApp.run(host='0.0.0.0', port=5000)
